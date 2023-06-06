ca135c10506656ad300dd219520ba3edaf1b077e
Devansh Upadhyay <devanshu9719@gmail.com>
2023-06-06 23:06:36 +0530

    Added basic stars to home page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,167 +1,98 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations'; 
+import * as THREE from 'three';
+import { WebGLRenderer, Scene } from 'three';
+// import * as dat from 'dat.gui';
 
-import { interval, timer, Observable } from 'rxjs';
-import { take, tap,switchMap } from 'rxjs/operators';
-
-import { HelperService } from '../helper.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  animations: [
-  trigger('appearFromBottom',[
-    transition('void => *',[
-      style({
-        transform: 'translateY(+40%)',
-        opacity : '0.6'
-      }),
-      animate('.2s ease')
-      ])
-    ]),
-  trigger('popout',[
-
-    transition(':enter',[
-      style({
-        opacity : '.1',
-        transform: 'scale(0.2)'
-
-      }),
-      animate('0.4s ease')
-      ])
-    ])
-  ]
 })
 export class AboutComponent implements OnInit {
-
-  public introductionText : string = '';
-  public academicText : string = '';
-  public introduction : string ='';
-  public academic : string ='';
-  public textEmitInterval : number = 20;
-  public showBlinker : boolean = false;
-  public skillsArray: Array<{[key:string] : string | number}>=[];
-  public skillSet: Array<{[key:string] : string | number}>=[];
-  public contactsList : Array<{[key:string] : string | number}>=[];
-  public contactVias : Array<{[key:string] : string | number}>=[];
-  public lightTheme: boolean = true;
-  constructor(private _helper: HelperService) { }
-
+  renderer!: WebGLRenderer;
+  scene!: THREE.Scene;
+  camera!: THREE.PerspectiveCamera;
+  starGeo!: THREE.BufferGeometry;
+  // dat = new dat.GUI();
+  sphere!: THREE.Points<THREE.TorusGeometry, THREE.PointsMaterial>;
+  loader!: THREE.TextureLoader;
+  particlesMesh!: THREE.Points<
+    THREE.BufferGeometry<THREE.NormalBufferAttributes>,
+    THREE.PointsMaterial
+  >;
   ngOnInit(): void {
+    this.initializeScene();
+    this.addTorusGeometry();
+    this.addParticles();
+    this.addPointLight();
+    this.render();
+    // console.log(dat);
+  }
 
-    this._helper.isLightTheme.subscribe((isLight:boolean) =>{
-      this.lightTheme = isLight;
+  initializeScene() {
+    this.renderer = new WebGLRenderer();
+    this.renderer.shadowMap.enabled = true;
+    this.loader = new THREE.TextureLoader();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+    this.scene = new Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      80,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    // this.camera.position.set(Math.PI / 2, 0, 1);
+    this.camera.position.set(0, 0, 2);
+  }
+  render() {
+    let component = this;
+    (function render() {
+      requestAnimationFrame(render);
+      component.animate();
+      component.renderer.render(component.scene, component.camera);
+    })();
+  }
+
+  addTorusGeometry() {
+    const torus = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
+    const material = new THREE.PointsMaterial({
+      color: 0xffffff,
+      size: 0.005,
+      // map: this.loader.load('assets/three/disc.png'),
     });
-
-
-    this.introductionText ="I'm Devansh Upadhyay, a web developer from India. Along with developing beautiful interfaces, I like reading books, about history & mythology, playing cricket & trying new things. Currently working in TCS, I develop interfaces for one of it's flagship products.";
-    this.contactsList = [
-    {
-      name : 'gmail',
-      url : 'mailto:devanshu9719@gmail.com'
-    },
-    {
-      name : 'linkedin',
-      url : 'https://linkedin.com/in/devanshhu'
-    },
-    {
-      name :'github',
-      url : 'https://github.com/devanshhu'
-    },
-    {
-      name: 'instagram',
-      url : 'https://instagram.com/devanshhu'
-    }
-    ];
-    this.skillsArray = [
-    {
-      id: 1,
-      name: "Higher Secondary",
-      colorName : 'yellow-500',
-      percentage:'100%',
-      description:'Scored 10/10 in 10 Std'
-    },
-    {
-      id: 2,
-      name: "Senior Secondary",
-      colorName : 'pink-500',
-      percentage:'92%',
-      description:'Scored 92.8% in 12 Std'
-    },
-    {
-      id: 3,
-      name: "Graduation",
-      colorName : 'blue-500',
-      percentage:'76%',
-      description:'Scored 7.6 GPA in B.Tech.'
-    }
-     ];
-    if(this._helper.isHomeVisited){
-      this.shortCut();
-    }else{
-      
-      this._startEmitting();
-    }
-    this._helper.isHomeVisited = true;
+    this.sphere = new THREE.Points(torus, material);
+    this.scene.add(this.sphere);
   }
-
-  private _startEmitting(){
-    
-    this._introText()
-    .pipe(
-      switchMap((x) => this._academyText())
-      )
-    .subscribe(
-      x => console.log(x),
-      err => console.log(err),
-      () => {
-        this.showBlinker = true;
-        this.populateContactMe();
-        
-      }
-      );
+  addPointLight() {
+    const pointLight = new THREE.PointLight(0xffffff, 0.1);
+    pointLight.position.set(2, 3, 4);
+    // this.scene.add(pointLight);
   }
-
-  private _introText(){
-
-    return interval(this.textEmitInterval)
-    .pipe(
-      take(this.introductionText.length),
-      tap((i) => this.introduction += this.introductionText[i])
-      );
+  animate() {
+    this.sphere.rotation.y += 0.01;
+    this.particlesMesh.rotation.y -= 0.01;
   }
-
-  private _academyText(){
-
-    return interval(this.textEmitInterval)
-    .pipe(
-      take(this.academicText.length),
-      tap((i) => this.academic += this.academicText[i])
-      );
-  }
-
-  private populateSkillSet(){
-
-    return interval(500).pipe(
-      take(3),
-      tap((i) => this.skillSet[i] = this.skillsArray[i])
-      );
-  }
-
-  private populateContactMe(){
-    interval(800).pipe(
-      take(4),
-      tap( (i) => this.contactVias[i] = this.contactsList[i])
-      ).subscribe(
-      x => console.log(x),
-      err => console.log(err),
-      () =>  this.populateSkillSet().subscribe()
-      );
+  addParticles() {
+    const particlesGeometry = new THREE.BufferGeometry();
+    const particlesCount = 5000;
+    const posArray = new Float32Array(particlesCount * 3);
+    for (let i = 0; i < particlesCount * 3; i++) {
+      posArray[i] = (Math.random() - 0.5) * 10;
     }
 
-    private shortCut(){
-      this.introduction = this.introductionText;
-      this.contactVias = this.contactsList;
-      this.skillSet = this.skillsArray;
-    }
+    particlesGeometry.setAttribute(
+      'position',
+      new THREE.BufferAttribute(posArray, 3)
+    );
+    const material = new THREE.PointsMaterial({
+      color: 0xffffff,
+      size: 0.005,
+      map: this.loader.load('assets/three/disc.png'),
+    });
+    this.particlesMesh = new THREE.Points(particlesGeometry, material);
+
+    this.scene.add(this.particlesMesh);
   }
+}
